282e7dc1c81c6c5b0b1cdf466974bffaeef13333
woodwhales <woodwhales@163.com>
2023-03-30 11:52:30 +0800

    drop useless code

--- src/main/java/org/woodwhales/music/Application.java
+++ src/main/java/org/woodwhales/music/Application.java
@@ -1,10 +1,8 @@
 package org.woodwhales.music;
 
-import cn.hutool.extra.spring.SpringUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.woodwhales.music.service.music.impl.MusicServiceImpl;
 
 @EnableTransactionManagement(proxyTargetClass=true)
 @SpringBootApplication
@@ -12,8 +10,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		MusicServiceImpl musicService = SpringUtil.getBean(MusicServiceImpl.class);
-		musicService.washLink();
 	}
 
 }
