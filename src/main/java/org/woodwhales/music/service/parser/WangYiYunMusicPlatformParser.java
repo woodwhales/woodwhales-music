11d49b2c55123e2e1ba221d7ea9f847e262aee54
woodwhales <woodwhales@163.com>
2023-09-05 18:35:09 +0800

    update wangyiyun parser

--- src/main/java/org/woodwhales/music/service/parser/WangYiYunMusicPlatformParser.java
+++ src/main/java/org/woodwhales/music/service/parser/WangYiYunMusicPlatformParser.java
@@ -1,6 +1,9 @@
 package org.woodwhales.music.service.parser;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -19,7 +22,19 @@ public class WangYiYunMusicPlatformParser extends BaseMusicPlatformParser {
     @Override
     protected String parseAlbum(Document document) {
         Elements elements = document.getElementsByClass("des s-fc4");
-        return elements.get(1).getElementsByClass("s-fc7").text();
+        Element element = elements.get(1).getElementsByClass("s-fc7").get(0);
+        String album = element.text();
+
+        String subAlbum = null;
+        Elements subAlbumElements = elements.get(1).getElementsByClass("s-fc8");
+        if(CollectionUtils.isNotEmpty(subAlbumElements)) {
+            subAlbum = subAlbumElements.get(0).text();
+            subAlbum = StringUtils.trim(subAlbum);
+        }
+        if(StringUtils.isNotBlank(subAlbum)) {
+            album = album + " " + subAlbum;
+        }
+        return album;
     }
 
     @Override
@@ -31,6 +46,16 @@ public class WangYiYunMusicPlatformParser extends BaseMusicPlatformParser {
     @Override
     protected String parseMusicTitle(Document document) {
         Elements titleElements = document.getElementsByClass("tit");
-        return titleElements.get(0).getElementsByClass("f-ff2").get(0).text();
+        String title = titleElements.get(0).getElementsByClass("f-ff2").get(0).text();
+        // 解析副标题
+        String subTitle = null;
+        Elements elementsByClass = titleElements.get(0).getElementsByClass("subtit f-fs1 f-ff2");
+        if(CollectionUtils.isNotEmpty(elementsByClass)) {
+            subTitle = elementsByClass.get(0).text();
+        }
+        if(StringUtils.isNotBlank(subTitle)) {
+            title = title + "(" + subTitle + ")";
+        }
+        return title;
     }
 }
