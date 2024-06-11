ffdfdeea688127bfa58b4fcd2cdeea4bf424a872
woodwhales <woodwhales@163.com>
2024-06-11 09:15:20 +0800

    fix: list data by pageSize

--- src/main/java/org/woodwhales/music/config/MybatisPlusConfig.java
+++ src/main/java/org/woodwhales/music/config/MybatisPlusConfig.java
@@ -17,8 +17,8 @@ public class MybatisPlusConfig {
         PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
         // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
         // paginationInterceptor.setOverflow(false);
-        // 设置最大单页限制数量，默认 500 条，-1 不受限制
-        paginationInnerInterceptor.setMaxLimit(100L);
+        // 设置最大单页限制数量，-1 不受限制
+        paginationInnerInterceptor.setMaxLimit(300L);
         // 开启 count 的 join 优化,只针对部分 left join
         paginationInnerInterceptor.setDbType(DbType.MYSQL);
         paginationInnerInterceptor.setOptimizeJoin(true);
